2a94d57f222356b53cee42dd0037736ebc49f2e5
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-18 17:43:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your solution here!
 const drivers=["Milo", "Otis", "Garfield"];
-destructivelyAppendDriver(drivers.push('Ralph'));
+destructivelyAppendDriver('Ralph');
+drivers.push(destructivelyAppendDriver);
